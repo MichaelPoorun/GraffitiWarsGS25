524fe11757fe04f54c2c5618acf75d31092a77b7
DuSamLam <102325897+DuSamLam@users.noreply.github.com>
2025-03-03 18:15:27 -0500

    health bar update

--- GraffitiWars/Assets/DuHealthBar.cs
+++ GraffitiWars/Assets/DuHealthBar.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
 public class DuHealthBar{
-    [SerializeField] private ImageConversion _healthbarsprite;
-
-    public void UpdateHealthBar(float maxHealth, float currentHealth);
-    _healthbarsprite.fillAmount = CurrentHealth/ MaxHealth;
+    [SerializeField] private ImageConversion
+    public HealthSystem hp;
+    public void Updatehp(float maxHealth, float currentHealth);
+    
+      
 }
