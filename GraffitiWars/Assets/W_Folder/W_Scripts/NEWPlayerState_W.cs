638df34c3daa6d0d36eba5a4b2ff16c683230f99
Wilx6 <144147441+Wilx6@users.noreply.github.com>
2025-03-05 05:59:52 -0500

    Statemachine Update
    Player can now attack, w/ animation (Gotta figure out how to make combo system with this new state machine)
    HP system implemented, so Player should be able to take damage now
    Player might be able to block, w/ animation (I have the code all done, but enemies isn't working for me so I have no way to test it, I'll fix it tmr)

--- GraffitiWars/Assets/W_Folder/W_Scripts/NEWPlayerState_W.cs
+++ GraffitiWars/Assets/W_Folder/W_Scripts/NEWPlayerState_W.cs
@@ -1,25 +1,43 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum PlayerState //Where all player states are kept
 {
     Idle,
     Walking,
-    Jumping
+    Attacking,
+    Jumping,
+    Blocking
 }
 
 public class NEWPlayerState_W : MonoBehaviour
 {
+    [Header("Player Variables")]
     public float speed; //Player Speed
     public float jumpPower;
 
-    private Rigidbody rb;
-
+    [Header("Player Bools")]
+    public bool currentlyAttacking = false;
     public bool isJumping = false;
-    public Animator animator;
+    public bool isBlocking = false;
 
+    [Header("Player Attack Hitboxes")]
+    public GameObject AttackBox1;
+
+    [Header("References")]
+    public HealthSystem HP;
+
+    [Header("Misc")]
+    private Rigidbody rb;
+    public Animator animator;
     public PlayerState currentState; //Variable that stores current state
 
-    private void Start()
+    void Awake()
+    {
+        AttackBox1.SetActive(false);
+    }
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
@@ -37,38 +55,61 @@ public class NEWPlayerState_W : MonoBehaviour
         switch (currentState)
         {
             case PlayerState.Idle:
-                Debug.Log("NEW Player is IDLE");
+                Debug.Log("NEW Player Is IDLE");
                 if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
                 {
                     ChangeState(PlayerState.Walking);
                 }
-                
                 if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
                 {
-                    animator.SetTrigger("isJumping"); //Triggers the animation based on the trigger that was made in the animator
                     ChangeState(PlayerState.Jumping);
                 }
-                    break;
+                if (Input.GetMouseButtonDown(0) && currentlyAttacking == false)
+                {
+                    ChangeState(PlayerState.Attacking);
+                }
+                if (Input.GetMouseButton(1))
+                {
+                    ChangeState(PlayerState.Blocking);
+                }
+                break;
 
             case PlayerState.Walking:
-                Debug.Log("NEW Player is Walking");
+                Debug.Log("NEW Player Is Walking");
                 PlayerMovement();
                 if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
                 {
                     ChangeState(PlayerState.Idle);
                 }
-                
                 if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
                 {
-                    animator.SetTrigger("isJumping"); //Triggers the animation based on the trigger that was made in the animator
                     ChangeState(PlayerState.Jumping);
+                }
+                if (Input.GetMouseButtonDown(0) && currentlyAttacking == false)
+                {
+                    ChangeState(PlayerState.Attacking);
                 }
                 break;
 
+            case PlayerState.Attacking:
+                Debug.Log("NEW Player Is Attacking");
+                StartCoroutine(Attack());
+                break;
+
             case PlayerState.Jumping:
-                Debug.Log("NEW PLAYER IS JUMPING");
+                Debug.Log("NEW Player Is Jumping");
                 isJumping = true;
                 Jumping();
+                break;
+
+            case PlayerState.Blocking:
+                Debug.Log("NEW Player Is Blocking");
+                Blocking();
+                if (Input.GetMouseButtonUp(1))
+                {
+                    isBlocking = false;
+                    ChangeState(PlayerState.Idle);
+                }
                 break;
         }
     }
@@ -76,10 +117,20 @@ public class NEWPlayerState_W : MonoBehaviour
     {
         currentState = newState;
         animator.SetBool("isWalking", newState == PlayerState.Walking); //SetBool makes the animator bool T or F. Same as doing if isWalking == true {play animation} else {iswalking == false}
+        animator.SetBool("isBlocking", newState == PlayerState.Blocking); //Set isBlocking to true in the animator
+
+        if (newState == PlayerState.Attacking)
+        {
+            animator.SetTrigger("isAttacking"); //Triggers the punch animation based on the trigger that was made in the animator
+        }
+        if (newState == PlayerState.Jumping)
+        {
+            animator.SetTrigger("isJumping"); //Triggers the jump animation based on the trigger that was made in the animator
+        }
     }
 
     //======================================================================//
-    //                          Player Movement                             //   
+    //                            Player Movement                           //   
     //======================================================================//
     public void PlayerMovement()
     {
@@ -89,9 +140,23 @@ public class NEWPlayerState_W : MonoBehaviour
     }
 
     //======================================================================//
+    //                            Player Attack                             //   
+    //======================================================================//
+    IEnumerator Attack()
+    {
+        Debug.Log("Player Has Attacked");
+        AttackBox1.SetActive(true);
+        currentlyAttacking = true;
+        yield return new WaitForSeconds(.55f);
+        AttackBox1.SetActive(false);
+        currentlyAttacking = false;
+        ChangeState(PlayerState.Idle);
+    }   
+
+    //======================================================================//
     //                             Player Jump                              //   
     //======================================================================//
-    void Jumping()
+    public void Jumping()
     {
         if (isJumping)
         {
@@ -107,4 +172,30 @@ public class NEWPlayerState_W : MonoBehaviour
             ChangeState(PlayerState.Idle);
         }
     }
+
+    //======================================================================//
+    //                             Player Block                             //   
+    //======================================================================//
+    public void Blocking()
+    {
+        isBlocking = true;
+    }
+
+    //======================================================================//
+    //                             Player Health                            //   
+    //======================================================================//
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hit_Player") && isBlocking == false)
+        {
+            Debug.Log("NEW Player Took 25 Damage");
+            HP.TakeDamage(HP.damage);
+        }
+        else if (isBlocking == true)
+        {
+            Debug.Log("NEW Player Negated Damage");
+            HP.damage = 0;
+        }
+    }
+
 }
