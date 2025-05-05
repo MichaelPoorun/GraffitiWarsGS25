47dd624b20ef6d9c22d76e6bab7d5d8246e86b8b
MichaelP <michaelpoorun@gmail.com>
2025-05-04 21:03:25 -0400

    Adding Audio Play file

---
+++ GraffitiWars/Assets/Scripts/Audio_Play.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class Audio_Play : MonoBehaviour
+{
+
+    AudioSource AS;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play_sound()
+    {
+        AS.Play();
+    }
+}
